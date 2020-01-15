8fa979ecde1788314c9f151f8ecdf81a379f89f2
Lola-D <lola.donval@gmail.com>
2020-01-15 17:38:42 +0100

    second commit

--- 03_stairway.rb
+++ 03_stairway.rb
@@ -1,55 +1,46 @@
+def roll_dice
+    return rand(6)+1
+end
 
+def analyze_dice(dice)
+    if dice >=5
+      puts "Vous avancez!"
+      return 1
+    elsif dice == 1
+      puts "Vous reculez!"
+      return -1
+    else
+      puts "rien ne se passe"
+      return 0
+    end
+end
 
+  def show_state(num)
+    puts "Vous êtes sur la marche n° #{num}"
+  end
 
+def is_over?(num)
+    if num == 10
+        return true
+    else
+        return false
+    end
+end
 
-# IN PROGRESS
+def play
+    puts "Bienvenue dans le jeu!!"
 
+    step = 1
+    show_state(step)
 
+    while(!is_over?(step)) do
+      puts "tapez 'entrée' pour jouer"
+      gets.chomp
+      step += analyze_dice(roll_dice)
+      show_state(step)
+    end
 
+    puts "===Vous avez gagné!==="
+end
 
-# marche = 0
-# nb_jets = 0
-# nb_parties = [] * 10
-
-# puts "Voici le jeu de l'escalier,"
-# puts "pour gagner, tu dois atteidre la dixième marche en lancant le dé :"
-# puts "si tu fais 5 ou 6 tu avances d'une marche"
-# puts "si tu fais 1 tu recules d'une case"
-# puts "si tu fais 2,3 ou 4 tu restes où tu es"
-# puts ""
-
-# #def jeu
-#     until marche == 10
-#         nb_jets += 1
-#         puts "Lance de dé !"
-#         print "Tu as fais : "
-#         puts jet = (1 + rand(6)).to_i
-#         if jet == 5 || jet == 6
-#             marche += 1
-#             puts "$$$ Bravo, tu avances d'une case. Tu te trouves désormais sur la marche #{marche}"
-#         elsif jet == 1 && marche >0
-#             marche -= 1
-#             puts "*** Zut ! Tu recules d'une case. Tu te trouves désormais sur la case #{marche}"
-#         else (jet == 1 && marche == 0) || (jet > 1 && jet < 5)
-#             puts "--- Tu ne bouges pas de la marche #{marche}."
-#         end
-#     end
-#     puts "°°° Tu as gagné ! Tu as atteint la dizième marche."
-#     puts "Il t'a fallut #{nb_jets} jets pour gagner."
-#     nb_parties.push nb_jets
-#     puts nb_parties
-#     puts nb_parties.length
-#     #return nb_parties
-# #end
-
-# # 10.times do
-# #     jeu
-# # end
-# # def repetitions(nb_parties)
-# #     10.time do
-# #         nb_parties
-# #     end
-# # end
-# # nb_parties = jeu
-# # repetitions(nb_parties)
-
+play
